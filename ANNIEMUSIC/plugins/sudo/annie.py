1fba1801a35b81db36693e985b594bbba71091ac
natha <108113193+Mandaiya@users.noreply.github.com>
2024-05-31 08:03:04 +0530

    Update annie.py

--- ANNIEMUSIC/plugins/sudo/annie.py
+++ ANNIEMUSIC/plugins/sudo/annie.py
@@ -27,10 +27,7 @@ Jarvis_text = [
 
 strict_txt = [
 "i can't restrict against my besties",
-"are you serious i am not restrict to my friends",
-"fuck you bsdk k mai apne dosto ko kyu kru",
-"hey stupid admin ", 
-"ha ye phele krlo maar lo ek dusre ki gwaand",  
+"are you serious i am not restrict to my friends",  
 "i can't hi is my closest friend",
 "i love him please don't restict this user try to usertand "
 ]
